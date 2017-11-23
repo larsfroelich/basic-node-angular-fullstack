36b111a9f6544633b322e91af5d68b023ff1503a
Lars Frölich <lars.froelich@gmail.com>
2017-11-23 12:58:22 +0100

    added "active" style to active header-element, updated body-parser

--- public/js/controllers/headerCtrl.js
+++ public/js/controllers/headerCtrl.js
@@ -1,10 +1,7 @@
-app.controller('headerCtrl', function($scope, $http) {
-    console.log("this is the headerctrl")
-
+app.controller('headerCtrl', function($scope, $http, $location) {
     $scope.headerItems = [
         {title: "Home", link: "/"},
         {title: "Link", link: "/link"}
     ];
-    $scope.currentHeaderItem = "";
-
+    $scope.currentHeaderItem = $scope.headerItems.findIndex(function(t){return t.link === $location.$$url;});
 });
